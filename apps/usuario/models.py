f03c75ca810b2f329057cf79d51de11da533f736
ManuelMarinDuque <Manuel.Duque5.2@gmail.com>
2019-03-20 10:53:39 -0500

    Creado y modificado el modelo/tabla usuario

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+import datetime
+
 
 # Create your models here.
+class Usuario(models.Model):
+
+    generos = (
+        ('M', 'Masculino'),
+        ('F', 'Femenino')
+    )
+
+    cedula = models.CharField(max_length=12,primary_key=True)
+    nombre_usuario = models.CharField(max_length=20)
+    password = models.CharField(max_length=12)
+    nombre = models.CharField(max_length=25)
+    apellido = models.CharField(max_length=25)
+    sexo = models.CharField(max_length=1, choices=generos)
+    correo = models.EmailField()
+    saldo = models.PositiveIntegerField()
+    telefono = models.CharField(max_length=10)
+    fecha_nacimiento = models.DateField()
+    nacionalidad = models.CharField(max_length=20)
+    ciudad = models.CharField(max_length=30)
+
+    def getedad(self):
+        return int((datetime.date.today() - self.fecha_nacimiento).days / 365.25)
+
+
