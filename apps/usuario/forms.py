32b9285fa9c09733c22214ecfe86edaf4cfa1084
ManuelMarinDuque <Manuel.Duque5.2@gmail.com>
2019-03-31 20:10:31 -0500

    Listo el registro de un usuario. Los campos se pasan uno por uno al 'registrar.html'.
    Issue lista: closes #2

--- apps/usuario/forms.py
+++ apps/usuario/forms.py
@@ -5,17 +5,23 @@ from apps.usuario.models import Usuario
 
 # Create your forms here:
 class RegistroForm(UserCreationForm):
+    generos = (
+        ('M', 'Masculino'),
+        ('F', 'Femenino')
+    )
+    sexo = forms.ChoiceField(choices=generos)
+
     class Meta:
         model = Usuario
         fields = [
-            'username',
-            'first_name',
-            'last_name',
-            'email',
-            'cedula',
-            'sexo',
-            'telefono',
-            'fecha_nacimiento',
-            'pais',
-            'ciudad',
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "cedula",
+            "sexo",
+            "telefono",
+            "fecha_nacimiento",
+            "pais",
+            "ciudad",
         ]
