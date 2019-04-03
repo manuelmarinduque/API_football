277772b5cb4290800c5fab5e24987c7d48b89cb1
ManuelMarinDuque <Manuel.Duque5.2@gmail.com>
2019-04-03 11:04:45 -0500

    Creada la función para ver los datos de un usuario.
    Cabe informar que en el base.html, en la parte de 'perfil', el link aún no está configurado hasta saber cómo configurar este acceso.

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.usuario.views import SignUpView
+from apps.usuario.views import SignUpView, UserDetailView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name="signup"),
+    path('detail/<int:pk>/', UserDetailView.as_view(), name='perfil')
 ]
