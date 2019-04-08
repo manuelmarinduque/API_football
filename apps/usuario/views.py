60433ec45bdbf1505444ffafee68ad2d3f231926
diegodelgado96 <diegodelgado045@gmail.com>
2019-04-08 10:22:28 -0500

    Dar estilos a los templates añadidos  closes #49

--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -79,5 +79,5 @@ class EmailUpdate(UpdateView):
     def get_form(self, form_class=None):
         form = super(EmailUpdate, self).get_form()
         form.fields['email'].widget = forms.EmailInput(
-            attrs={'placeholder': 'Correo electrónico'})
+            attrs={'class': 'form-control mb-2','placeholder': 'Correo electrónico'})
         return form
---
+++ static/js/configuracion.js
@@ -0,0 +1,4 @@
+document.getElementById("id_username").readOnly = true;
+document.getElementById("id_email").readOnly = true;
+document.getElementById("id_cedula").readOnly = true;
+document.getElementById("id_saldo").readOnly = true;
